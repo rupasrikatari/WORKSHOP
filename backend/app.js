1b30642debc6b96029965f91012371d5b5c9b90d
Jagritisinghh <jagritisinghAgh>
2025-02-11 08:18:27 +0530

    cors

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const connectDB= require('./config/db');
+const cors=require("cors");
 
 const dotenv =require('dotenv');
 dotenv.config();
 
+
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // connectDb
 connectDB();
